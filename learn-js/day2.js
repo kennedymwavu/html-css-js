a14e79b42d4d4d9e2e1e1bd52274ee4d7712be7a
KennedyMwavu <mwavukennedy@gmail.com>
2022-02-16 22:44:12 +0300

    Hello World, but in JS

---
+++ learn-js/day2.js
@@ -0,0 +1,19 @@
+// First things first:
+console.log("Hello World!");
+
+// Alright, let's declare some variables:
+var a = 1;
+console.log(a);
+
+// With var we can redeclare vars more than once:
+var a = 2;
+console.log(a);
+
+// If we do not want to redeclare a var more than once we can use 'let':
+let b = 3;
+console.log(b);
+
+// Now if we try to redeclare b again an error is raised: SyntaxError: Identifier 'b' has already been declared
+// let b = 2;
+// console.log(b)
+
